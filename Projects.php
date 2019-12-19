881dbc1e886ea5e8918dbc84fef00dd8fa0de7c5
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2019-12-19 19:26:26 +0200

    Create Projects.php

---
+++ Projects.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <title>Project info</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <style>
+
+        </style>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Projects</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <button class="btn btn-primary float-right m-2">Add Project</button>
+                    <button class="btn btn-primary float-right m-2">Add Member</button>
+                </div>
+                <div class="col-12">
+                    <table class="table">
+						<tr>
+                            <th>Project ID</th>
+                            <th>Project name</th>
+                            <th>Hours Per Day</th>
+                            <th>Start date</th>
+                            <th>End date</th>
+                            <th>Cost</th>
+                            <th>Deliverables</th>
+                        </tr>
+                        
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <script src="js/jquery.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+
+</html>
