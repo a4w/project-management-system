2c73acc576ffe995faa244759bcb839b0bbbacaa
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2019-12-19 17:43:47 +0200

    Database connection file

---
+++ db.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+const DB_HOST = '127.0.0.1';
+const DB_USER = 'root';
+const DB_PASSWORD = '12341234';
+const DB_NAME = 'project-management-system';
+
+$link = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
