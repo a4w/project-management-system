cc44a2b7ed92fd437e8284afcab47acf6ff812f3
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2019-12-19 17:40:04 +0200

    Initial set-as-complete task button

--- project_info.php
+++ project_info.php
@@ -34,18 +34,30 @@
                 </div>
                 <div class="col-12">
                     <table class="table">
-                        <th>
-                            <td>Task ID</td>
-                            <td>Task name</td>
-                            <td>Assigne</td>
-                            <td>Start date</td>
-                            <td>End date</td>
-                            <td>Working hours</td>
-                            <td>Predecessors</td>
-                            <td>Is Milestone</td>
-                            <td>Status</td>
-                            <td>Action</td>
-                        </th>
+                        <tr>
+                            <th>Task ID</th>
+                            <th>Task name</th>
+                            <th>Assigne</th>
+                            <th>Start date</th>
+                            <th>End date</th>
+                            <th>Working hours</th>
+                            <th>Predecessors</th>
+                            <th>Is Milestone</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Create project</td>
+                            <td>Sara Samer <small>(CEO)<small></td>
+                            <td>2019-12-24</td>
+                            <td>2019-12-30</td>
+                            <td>450</td>
+                            <td>1,5,9</td>
+                            <td>YES</td>
+                            <td>Pending</td>
+                            <td><button class="btn btn-sm btn-warning set-as-complete" data-target="1">Set as complete</button></td>
+                        </tr>
                     </table>
                 </div>
             </div>
@@ -54,6 +66,16 @@
         <script src="js/jquery.min.js"></script>
         <script src="js/popper.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
+        <script>
+
+            $('.set-as-complete').click(function(){
+                const id = $(this).attr("data-target");
+                const working_hours = prompt("Enter number of hours");
+                // Send to server that task {id} is done
+                window.location.reload(true);
+            });
+
+        </script>
     </body>
 
 </html>
