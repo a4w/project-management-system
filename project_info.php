19ca0724ed9d4c0ad8bdd65617a40436ba182a41
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2019-12-19 17:25:59 +0200

    Project info file

---
+++ project_info.php
@@ -0,0 +1,59 @@
+<html>
+    <head>
+        <title>Project info</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <style>
+
+        </style>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Project Name</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <span>ID: 0</span>
+                </div>
+                <div class="col-12">
+                    <span>NAME: NAME</span>
+                </div>
+                <div class="col-12">
+                    <span>Start date: NAME</span>
+                </div>
+                <div class="col-12">
+                    <span>Due date: </span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <button class="btn btn-primary float-right m-2">Add Task</button>
+                </div>
+                <div class="col-12">
+                    <table class="table">
+                        <th>
+                            <td>Task ID</td>
+                            <td>Task name</td>
+                            <td>Assigne</td>
+                            <td>Start date</td>
+                            <td>End date</td>
+                            <td>Working hours</td>
+                            <td>Predecessors</td>
+                            <td>Is Milestone</td>
+                            <td>Status</td>
+                            <td>Action</td>
+                        </th>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <script src="js/jquery.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+
+</html>
