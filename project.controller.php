b71f47b6a6f61b3cecc8f4369754f23c90d765b1
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2019-12-20 18:25:19 +0200

    save task to database added

--- project.controller.php
+++ project.controller.php
@@ -1,5 +1,6 @@
 <?php
 require 'db.inc.php';
+ini_set('display_errors', true);
 
 $action = isset($_POST['action']) ? $_POST['action'] : null;
  
@@ -17,4 +18,18 @@ switch($action){
         $stmt->bind_param('s', $member_name);
         $stmt->execute();
         break;
+    case "add-task":
+        $pid = $_POST['project-id'];
+        $task_name = $_POST['task-name'];
+        $start_date = $_POST['start-date'];
+        $end_date = $_POST['end-date'];
+        $working_hrs = $_POST['working-hrs'];
+        $milestone = isset($_POST['milestone']);
+        $predecessors[] = $_POST['predecessors'];
+        $parent = $_POST['parent'];
+        $stmt = $link->prepare('INSERT INTO `task` (`name`, `start-date`, `end-date`, `working-hours`, `parent-task-id`, `is-milestone`, `project-id`) VALUES (?,?,?,?,NULLIF(?,0),?,?)');
+        $stmt->bind_param('sssiiii',$task_name, $start_date, $end_date, $working_hrs, $parent, $milestone, $pid);
+        $stmt->execute();
+        header('Location:Project_info.php?id='.$pid);
+    break;
 }
