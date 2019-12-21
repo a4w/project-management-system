0487eadf57051de2583b0380c3bad3e261670a52
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2019-12-21 16:35:02 +0200

    database updated

--- project-management-system.sql
+++ project-management-system.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 19, 2019 at 06:06 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.0
+-- Generation Time: Dec 21, 2019 at 02:33 PM
+-- Server version: 8.0.17
+-- PHP Version: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,7 +31,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `deliverables` (
   `id` int(11) NOT NULL,
   `project-id` int(11) NOT NULL,
-  `title` text COLLATE utf8_bin NOT NULL
+  `title` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -39,8 +39,8 @@ CREATE TABLE `deliverables` (
 --
 
 INSERT INTO `deliverables` (`id`, `project-id`, `title`) VALUES
-(1, 1, 'Software application'),
-(2, 1, 'Training plan');
+(3, 1, 'software Program'),
+(4, 1, 'Training Plan');
 
 -- --------------------------------------------------------
 
@@ -50,7 +50,7 @@ INSERT INTO `deliverables` (`id`, `project-id`, `title`) VALUES
 
 CREATE TABLE `member` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8_bin NOT NULL
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -69,19 +69,20 @@ INSERT INTO `member` (`id`, `name`) VALUES
 
 CREATE TABLE `project` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `pm-id` int(11) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `hours-per-day` int(11) NOT NULL,
   `cost` double NOT NULL,
   `start-date` date NOT NULL,
-  `end-date` date NOT NULL
+  `end-date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `project`
 --
 
-INSERT INTO `project` (`id`, `name`, `hours-per-day`, `cost`, `start-date`, `end-date`) VALUES
-(1, 'Project 1', 8, 500000000, '2019-12-24', '2019-12-30');
+INSERT INTO `project` (`id`, `pm-id`, `name`, `hours-per-day`, `cost`, `start-date`, `end-date`) VALUES
+(1, 1, 'Project - 1', 5, 10000000, '2019-12-01', '2019-12-09');
 
 -- --------------------------------------------------------
 
@@ -91,10 +92,18 @@ INSERT INTO `project` (`id`, `name`, `hours-per-day`, `cost`, `start-date`, `end
 
 CREATE TABLE `project-managers` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8_bin NOT NULL,
-  `username` varchar(50) COLLATE utf8_bin NOT NULL,
-  `password` varchar(50) COLLATE utf8_bin NOT NULL
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `project-managers`
+--
+
+INSERT INTO `project-managers` (`id`, `name`, `username`, `password`) VALUES
+(1, 'Sara', 'Admin', '1234'),
+(2, 'Ahmed', 'user', '123123');
 
 -- --------------------------------------------------------
 
@@ -105,7 +114,7 @@ CREATE TABLE `project-managers` (
 CREATE TABLE `project-member-titles` (
   `project-id` int(11) NOT NULL,
   `member-id` int(11) NOT NULL,
-  `title` varchar(255) COLLATE utf8_bin NOT NULL
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -116,14 +125,14 @@ CREATE TABLE `project-member-titles` (
 
 CREATE TABLE `task` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `start-date` date NOT NULL,
   `end-date` date NOT NULL,
-  `working-hours` int(11) NOT NULL DEFAULT 0,
+  `working-hours` int(11) NOT NULL DEFAULT '0',
   `parent-task-id` int(11) DEFAULT NULL,
-  `is-complete` tinyint(1) NOT NULL DEFAULT 0,
-  `actual-working-hours` int(11) NOT NULL DEFAULT 0,
-  `is-milestone` tinyint(1) NOT NULL DEFAULT 0,
+  `is-complete` tinyint(1) NOT NULL DEFAULT '0',
+  `actual-working-hours` int(11) NOT NULL DEFAULT '0',
+  `is-milestone` tinyint(1) NOT NULL DEFAULT '0',
   `project-id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -132,7 +141,8 @@ CREATE TABLE `task` (
 --
 
 INSERT INTO `task` (`id`, `name`, `start-date`, `end-date`, `working-hours`, `parent-task-id`, `is-complete`, `actual-working-hours`, `is-milestone`, `project-id`) VALUES
-(1, 'Task A', '2019-12-19', '2019-12-19', 20, 1, 1, 50, 1, 1);
+(1, 'Task A', '2019-12-11', '2019-12-18', 20, NULL, 0, 0, 0, 1),
+(2, 'Task B', '2019-12-02', '2019-12-09', 3, NULL, 0, 0, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -162,8 +172,8 @@ CREATE TABLE `task-members` (
 --
 
 INSERT INTO `task-members` (`task-id`, `member-id`, `working-hours`) VALUES
-(1, 1, 10),
-(1, 2, 20);
+(2, 1, 0),
+(2, 2, 0);
 
 --
 -- Indexes for dumped tables
@@ -186,7 +196,8 @@ ALTER TABLE `member`
 -- Indexes for table `project`
 --
 ALTER TABLE `project`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pm-project` (`pm-id`);
 
 --
 -- Indexes for table `project-managers`
@@ -220,6 +231,7 @@ ALTER TABLE `task-dependency`
 -- Indexes for table `task-members`
 --
 ALTER TABLE `task-members`
+  ADD PRIMARY KEY (`task-id`,`member-id`),
   ADD KEY `task-id` (`task-id`),
   ADD KEY `member-id` (`member-id`);
 
@@ -231,7 +243,7 @@ ALTER TABLE `task-members`
 -- AUTO_INCREMENT for table `deliverables`
 --
 ALTER TABLE `deliverables`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `member`
@@ -243,19 +255,19 @@ ALTER TABLE `member`
 -- AUTO_INCREMENT for table `project`
 --
 ALTER TABLE `project`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `project-managers`
 --
 ALTER TABLE `project-managers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `task`
 --
 ALTER TABLE `task`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -265,7 +277,13 @@ ALTER TABLE `task`
 -- Constraints for table `deliverables`
 --
 ALTER TABLE `deliverables`
-  ADD CONSTRAINT `pid-delivery` FOREIGN KEY (`project-id`) REFERENCES `project` (`id`);
+  ADD CONSTRAINT `pid-delivery` FOREIGN KEY (`project-id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `project`
+--
+ALTER TABLE `project`
+  ADD CONSTRAINT `pm-project` FOREIGN KEY (`pm-id`) REFERENCES `project-managers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `project-member-titles`
@@ -278,7 +296,7 @@ ALTER TABLE `project-member-titles`
 -- Constraints for table `task`
 --
 ALTER TABLE `task`
-  ADD CONSTRAINT `pid` FOREIGN KEY (`project-id`) REFERENCES `project` (`id`);
+  ADD CONSTRAINT `pid` FOREIGN KEY (`project-id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `task-dependency`
