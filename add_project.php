e9e00d73aeb7d71c408e993af2aebe6aef8e3b8f
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2019-12-21 21:50:10 +0200

    Changed add project title

--- add_project.php
+++ add_project.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8" />
-    <title>web zero</title>
+    <title>Add new project</title>
     <link rel='stylesheet' href='css/bootstrap.min.css' />
     <link rel='stylesheet' href='css/style.css' />
 
