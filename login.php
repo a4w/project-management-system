5533f7b9a70d4a6a6a05a9d050098605af11bdbb
aya-mahmoud <36283963+aya-mahmoud@users.noreply.github.com>
2019-12-23 01:55:31 +0200

    login styling

--- login.php
+++ login.php
@@ -11,34 +11,41 @@
 </head>
 
 <body>
-    <form method="post" name="loginForm" onsubmit="return login()" action="Projects.php">
-        <div class="container">
+<div class="loginpage">
+        <div class="image float-left">
+            <img src="imgs/pm.jpg" alt="project_managers">
+        </div>
+        <form method="post" name="loginForm" onsubmit="return login()" action="Projects.php" class="float-left">
+        <h2 class="header"> Project manager</h2>
+        <div class="container col-md-pull-2  ">
             <h1>Login :</h1>
 
             <div class="row">
-                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="col-lg-5 ">
                     <div class="label"> User Name </div>
                 </div>
 
-                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="col-lg-5 ">
                     <input type="text" name="userName">
                 </div>
             </div>
 
             <div class="row">
-                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="col-lg-5 ">
                     <div class="label"> Password </div>
                 </div>
 
-                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="col-lg-5 ">
                     <input type="password" name="password">
                 </div>
             </div>
-        </div>
-
-        <input class="btn btn-danger float-left" type="submit" value="Login">
-
+     
+  
+        <input class="btn btn-danger " type="submit" value="Login">
+     </div>
     </form>
+     <div class="float-fix"></div>
+</div>    
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/js.js"></script>
