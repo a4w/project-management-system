a69267c104d0dba71d8d4256a5e4735fac4c0e19
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2019-12-22 04:59:09 +0200

    Typo

--- Projects.php
+++ Projects.php
@@ -36,7 +36,7 @@ $plan_cfg = json_decode(file_get_contents('plan_cfg.json'), true);
                     <div class="modal-dialog">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h4 class="modal-title">Plan Configration</h4>
+                                <h4 class="modal-title">Plan Configuration</h4>
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                             </div>
                             <div class="modal-body">
