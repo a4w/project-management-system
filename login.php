95f51269af4ca2bb4ee711ada2400b51a49f4fe5
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2019-12-30 20:33:10 +0200

    Revert "login styling"
    This reverts commit 5533f7b9a70d4a6a6a05a9d050098605af11bdbb.

--- login.php
+++ login.php
@@ -11,41 +11,34 @@
 </head>
 
 <body>
-<div class="loginpage">
-        <div class="image float-left">
-            <img src="imgs/pm.jpg" alt="project_managers">
-        </div>
-        <form method="post" name="loginForm" onsubmit="return login()" action="Projects.php" class="float-left">
-        <h2 class="header"> Project manager</h2>
-        <div class="container col-md-pull-2  ">
+    <form method="post" name="loginForm" onsubmit="return login()" action="Projects.php">
+        <div class="container">
             <h1>Login :</h1>
 
             <div class="row">
-                <div class="col-lg-5 ">
+                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
                     <div class="label"> User Name </div>
                 </div>
 
-                <div class="col-lg-5 ">
+                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
                     <input type="text" name="userName">
                 </div>
             </div>
 
             <div class="row">
-                <div class="col-lg-5 ">
+                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
                     <div class="label"> Password </div>
                 </div>
 
-                <div class="col-lg-5 ">
+                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
                     <input type="password" name="password">
                 </div>
             </div>
-     
-  
-        <input class="btn btn-danger " type="submit" value="Login">
-     </div>
+        </div>
+
+        <input class="btn btn-danger float-left" type="submit" value="Login">
+
     </form>
-     <div class="float-fix"></div>
-</div>    
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/js.js"></script>
